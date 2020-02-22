01737c672c9182c61cb46d4e0ae6216451099b1a
Alexander Sessa <alexander.sessa@gmail.com>
2020-02-22 06:25:51 +0100

    removed test out

--- comments/save.php
+++ comments/save.php
@@ -10,7 +10,6 @@ $redis->connect($redis_host, $redis_port);
 if (isset($_POST['hash']) && ($_POST['hash'] == crypt($_POST['user'], $salt)))
   $redis->hset('content:' . $_POST['id'], 'c_text', urldecode($_POST['c_text']));
 
-file_put_contents('test/c_text', urldecode($_POST['c_text']));
 $redis->close();
 exit();
 ?>
